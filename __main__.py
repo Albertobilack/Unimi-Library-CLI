ea8e2201173921e81de03f7e0f0c29407aab6f38
Alberto Bino <binoalberto02@gmail.com>
2024-08-10 03:38:30 +0200

    Add "wait_start" function to enable "book_library" execution at midnight
    integration of "wait_start" with "book_library" hasn't been tested yet

--- __main__.py
+++ __main__.py
@@ -2,8 +2,11 @@
 
 import argparse
 from easystaff import Easystaff
+from time import sleep
+from datetime import datetime
+import pytz
 
-def list_biblio(args): 
+def list_library(args): 
     a = Easystaff()
     groundLibrary, firstLibrary = a.get_list()
 
@@ -32,12 +35,7 @@ def list_biblio(args):
                 contatore += 1
 
 
-# da cambiare presentazione delle informazioni : se slot["reserved"] = false scrivo a terminale prenotabile
-# se slot["reserved"] = true scrivo a terminale non prenotabile
-#DA CONTROLLARE COSA SUCCEDE SE POSTI PRENOTABILI PER UN ORARIO SONO 0, SE VENGONO MOSTRATI COMUNQUE E QUINDI VANNO NOTIFICATI COME VUOTI O VENGONO COMPLMETAMENTE
-#ESCLUSI DAL JSON
-#potrebbe essere utilizzata per mostrare le prenotazoini attive
-def freespot_biblio(args): 
+def freespot_library(args): 
     a = Easystaff()
     groundLibrary, firstLibrary = a.get_freespot(args.tf)
     groundLibrary = (groundLibrary["schedule"])
@@ -56,14 +54,26 @@ def freespot_biblio(args):
         if slot["disponibili"] > 0:
             print(orario, "| active reservation:", slot["reserved"])
 
+
+def wait_start():
+    startTime = "00:10"
+    startTime = datetime.strptime(startTime, "%H:%M").time()
+    cet = pytz.timezone("CET")
+    while startTime > datetime.now(cet).time():
+        sleep(30)
+
+
 # da fare bookbiblio senza login, solo con email e cf
 # posso collegare bookbiblio con freespoto o getbiblio e controllare se orari sono prenotabili prima di tentare
 #la prenotazione
-def book_biblio(args):
+def book_library(args):
+
+    if not args.now :
+        wait_start()
 
     a = Easystaff()
     a.login()
-    a.get_book(args.day, args.start, args.end, args.floor)
+    a.get_book(args.day, args.start, args.end, args.floor, args.now)
     # da definire piano, per ora preimpostato su piano terra
     # da sistemare input per orario e giorno
 
@@ -91,7 +101,7 @@ if __name__ == "__main__":
     #todo minore : mostrare solo determinate fasce orarie in base a quante ore prenotare?
     biblio_list = sub.add_parser("list", help="lista degli orari liberi di tutti i posti disponibili per entrambi i piani")
     #biblio_l.add_argument("-piano", help="piano da visualizzare", required=True)
-    biblio_list.set_defaults(func=list_biblio)
+    biblio_list.set_defaults(func=list_library)
 
 # idea:
 #nota : giorno da prenotare selezionato in base alla posizione dell'array, quindi list deve
@@ -103,15 +113,16 @@ if __name__ == "__main__":
     biblio_book.add_argument("-floor", help="piano da prenotare: ground | first", required=True, choices=["ground", "first"])
     biblio_book.add_argument("-start", help="ora inizio prenotazione, formato H:M", required=True) # provare ad aggiungere type=datetime.strftime("%Y-%m-%d")
     biblio_book.add_argument("-end", help="ora fine prenotazione, formato H:M", required=True)
+    biblio_book.add_argument("-now", help="reserve your spot instantly instead of waiting until midnight", action=argparse.BooleanOptionalAction)
     #biblio_book.add_argument("-u", "--username", dest="u", metavar=None, help="email di istituto", required=True) #da aggiungere default, da sistemare metavar
     #biblio_book.add_argument("-p", "--password", dest="p", metavar=None, help="password di istituto", required=True)
-    biblio_book.set_defaults(func=book_biblio)
+    biblio_book.set_defaults(func=book_library)
 
     biblio_freespot = sub.add_parser("freespot", help="lista delle fasce orarie prenotaibli in un determinato timeframe, viene precisato se già prenotate o prenotabili se indicato il CF dell'utente")
     biblio_freespot.add_argument("-tf", help="ampiezza della fascia oraria da visualizzare in ore; default = 1 ", required=False, type=int, default=1)
     #biblio_freespot.add_argument("-day", help="giorno da visualizzare", required=True)
     #biblio_freespot.add_argument("-piano", help="piano da visualizzare", required=True)
-    biblio_freespot.set_defaults(func=freespot_biblio)
+    biblio_freespot.set_defaults(func=freespot_library)
 
 
     args = parser.parse_args()
